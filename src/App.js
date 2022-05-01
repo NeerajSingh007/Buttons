f566b06b956d1843a443c1a383fbeb0f6010660d
Neeraj Singh Rajput <78468513+NeerajSingh007@users.noreply.github.com>
2022-05-01 13:08:54 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import { Button } from './components/button'
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button primary border="none" color='white'>
+        Primary Button
+      </Button>
+      <br />
+      <Button>
+        Default Button
+      </Button>
+      <br />
+      <Button border="dashed">Dashed Button</Button>
+      <br />
+      <Button border="none">Text Button</Button>
+      <br />
+      <Button border="none" color='dodgerblue'>Link Button</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
